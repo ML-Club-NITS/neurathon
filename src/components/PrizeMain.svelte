d5fc884c5aad354291e372e0ca0a966ac5eeabad
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-01-12 18:05:24 +0530

    upd: responsiveness in prizes

--- src/components/PrizeMain.svelte
+++ src/components/PrizeMain.svelte
@@ -25,7 +25,13 @@
 	];
 </script>
 
+
+
+<section
+	class="my-32 flex w-full items-center justify-center border-slate-500 bg-black px-5 md:px-24 lg:py-1"
+>
 <div class="prize-container my-12 py-8">
+	
 	<span
 		class="inline-flex w-fit animate-text-gradient text-wrap bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] bg-clip-text p-2 text-center text-4xl font-bold text-transparent sm:text-5xl md:text-6xl"
 	>
@@ -39,6 +45,7 @@
 		{/each}
 	</div>
 </div>
+</section>
 
 <style>
 	.prize-container {
@@ -59,5 +66,34 @@
 		transition:
 			transform 0.3s ease,
 			boxw 0.3s ease;
+	}
+
+	@media (max-width: 1024px) {
+		.prize-content {
+			grid-template-columns: repeat(2, 1fr);
+			gap: 30px;
+		}
+	}
+
+	@media (max-width: 768px) {
+		.prize-content {
+			grid-template-columns: 1fr;
+			gap: 65px;
+		}
+		.prize-wrapper {
+			transform: translateY(0);
+		}
+		.prize-container {
+			row-gap: 65px;
+		}
+	}
+
+	@media (max-width: 480px) {
+		.prize-content {
+			gap: 65px;
+		}
+		.prize-container span {
+			font-size: 1.5rem;
+		}
 	}
 </style>
