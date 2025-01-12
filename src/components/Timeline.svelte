0216c3fb0b96b1c99270639b3ac148f5f1b0b676
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-01-12 10:10:03 +0530

    changes

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Timeline from './ui/TimelineComponent.svelte';
+	import Content1 from './timeLineContents/Content1.svelte';
+	import Content2 from './timeLineContents/Content2.svelte';
+	import Content3 from './timeLineContents/Content3.svelte';
+	import RetroGrid from './RetroGrid.svelte';
+
+	let timelineData: any = [
+		{
+			title: 'Svelte 5',
+			content: Content1
+		},
+		{
+			title: 'Early 2023',
+			content: Content2
+		},
+		{
+			title: 'Changelog',
+			content: Content3
+		}
+	];
+</script>
+
+<section
+	id="timeline"
+	class="flex h-full w-full flex-wrap justify-around overflow-hidden bg-white font-sans md:px-10 dark:bg-neutral-950"
+>
+	<Timeline {timelineData} />
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+	import CustomCard from '../components/CustomCard.svelte';
+	import Prizes from '../components/Prizes.svelte';
+	import Prizes2 from '../components/Prizes2.svelte';
+	import Timeline from '../components/Timeline.svelte';
+
 	let { data } = $props();
 </script>
 
-<h1 class="text-4xl">Welcome to Supabase!</h1>
-<p>Last fetch Server Time: {data.currentTime}</p>
+<!-- <h1 class="text-4xl">Welcome to Supabase!</h1>
+<p>Last fetch Server Time: {data.currentTime}</p> -->
+
+<Prizes2 />
+<Timeline />
+<CustomCard />
