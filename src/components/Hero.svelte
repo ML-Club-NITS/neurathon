72866c98eb93890515a4b12ad63dd2c37db0f539
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-01-13 16:53:51 +0530

    Hero modified

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -4,18 +4,20 @@
 
 <section>
 	<!-- <img src={bg1} alt=""> -->
-	
-	<div
-		class="flex h-screen w-full items-center justify-between text-4xl text-white"
-	>
-	<div class="ml-20">
-		<div>Neurathon 2025</div>
-		<div class="text-lg">Where Creativity Meets Innovation</div>
-	</div>
-	<div class="w-auto h-screen">
-		<div class="bg-blend-lighten w-40 h-full bg-gradient-to-r from-black to-transparent absolute"></div>
-		<img src="bg1.gif" alt="" class="bg-blend-difference">
-	</div>
+
+	<div class="flex h-auto w-full items-center justify-between text-4xl text-white flex-wrap-reverse lg:flex-nowrap">
+		<div class="h-auto mt-20 lg:mt-0 lg:ml-20 text-center lg:items-start flex flex-col justify-center items-center w-full lg:w-auto">
+			<div class="text-2xl font-bold md:text-4xl">Neurathon 2025</div>
+			<div class="text-base font-thin text-gray-400 md:text-lg">
+				Where Creativity Meets Innovation
+			</div>
+		</div>
+		<div class="h-auto w-full lg:w-auto">
+			<div
+				class="absolute h-full w-40 bg-gradient-to-r from-black to-transparent bg-blend-lighten"
+			></div>
+			<img src="bg1.gif" alt="" class="bg-blend-difference w-full" />
+		</div>
 	</div>
 	<div class="absolute h-full w-full bg-gradient-to-t from-black to-transparent"></div>
 </section>
