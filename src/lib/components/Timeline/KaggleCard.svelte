b4e4b62c16f7aee8734055a92a5a7455452c48fc
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-01-15 17:08:38 +0530

    Kaggle card color

--- src/lib/components/Timeline/KaggleCard.svelte
+++ src/lib/components/Timeline/KaggleCard.svelte
@@ -6,11 +6,11 @@
   <div class="flex w-full flex-col gap-4 h-[250px] lg:flex-row">
     <MagicCard
       class="cursor-pointer flex-col items-center justify-center shadow-2xl whitespace-nowrap text-4xl group hover:border-[#392b879e] transition-all duration-300"
-      gradientColor="#44a6c6"
+      gradientColor="#44a6c5"
       gradientSize={300}
     >
       <div
-        class="group-hover:text-[#2961a1] transition-all duration-500 flex gap-1.5 justify-center items-center font-semibold"
+        class="group-hover:text-[#2130428a] transition-all duration-500 flex gap-1.5 justify-center items-center font-semibold"
       >
         <img src={kaggleSvg} alt="kaggle_logo" class="size-40 mt-1.5" />
       </div>
