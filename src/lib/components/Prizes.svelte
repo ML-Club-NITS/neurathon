e76a97fb33e0e1e8b974991ddd52635b9b8d585d
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-01-15 17:08:58 +0530

    fixes

--- src/lib/components/Prizes.svelte
+++ src/lib/components/Prizes.svelte
@@ -9,7 +9,7 @@
 >
 	<div class="prize-container flex-col items-center justify-center gap-y-7 ">
 		<span
-			class="inline-flex w-fit animate-text-gradient text-wrap bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] bg-clip-text p-2 text-center text-2xl font-bold text-transparent md:text-3xl lg:text-5xl xl:text-8xl"
+			class="inline-flex w-full justify-center mb-4 animate-text-gradient text-wrap bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] bg-clip-text p-2 !text-center text-2xl font-bold text-transparent md:text-3xl lg:text-5xl xl:text-8xl"
 		>
 			Prizes
 		</span>
