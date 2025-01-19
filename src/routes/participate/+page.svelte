72c08cbd59527acab01e96154cbb0b89408db1bd
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-01-19 19:00:21 +0530

    formatted

--- src/routes/participate/+page.svelte
+++ src/routes/participate/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import Participate from "$lib/components/Participate.svelte";
+	import Participate from '$lib/components/Participate.svelte';
 	import type { PageData } from './$types';
 	let { data }: { data: PageData } = $props();
 </script>
 
-
-<section class="flex bg-green-500 justify-evenly">
-	<Participate {data}/>
+<section class="flex justify-evenly bg-green-500">
+	<Participate {data} />
 </section>
