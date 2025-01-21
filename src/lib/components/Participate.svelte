1248f52c7eacb1fe243391d33703e8b1fc97bc07
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-01-22 00:55:25 +0530

    upd: participate

--- src/lib/components/Participate.svelte
+++ src/lib/components/Participate.svelte
@@ -7,7 +7,7 @@
 	let eligData2 = [
 		{
 			title: 'Registrations',
-			des: '12',
+			des: '--',
 			icon: 'fi fi-ss-team-check-alt'
 		},
 		{
@@ -29,7 +29,7 @@
 	<div
 		class="text-whit mt-[4.3rem] flex flex-col gap-5 gap-x-0 md:flex-row lg:flex-row xl:flex-row"
 	>
-		<div class="reg g-1 text-white md:w-[50%] lg:w-[50%] xl:w-[50%]">
+		<div class="reg g-1 text-white md:w-[40%] lg:w-[40%] xl:w-[40%]">
 			<div
 				class="reg-container m-3 flex flex-col justify-center overflow-hidden rounded border border-gray-600 bg-gray-700/[0.7]"
 			>
@@ -46,7 +46,7 @@
 						<i class="fi fi-br-calendar-day"></i>
 						<i class="text-[0.7rem]">{neurathonDate}</i>
 					</div>
-					<div class="event-text text-wrap">
+					<div class="event-text text-wrap text-[0.95rem] ">
 						Neurathon is a distinguished machine learning hackathon designed for the development of
 						creative AI solutions within an intensive, collaborative environment. Participants form
 						teams to tackle challenges curated by the organizers, focusing on areas such as software
@@ -62,9 +62,9 @@
 				</div>
 			</div>
 			<div
-				class="eligibility-container m-3 flex flex-col gap-2 rounded border border-gray-600 bg-gray-700/[0.7] p-5"
+				class="eligibility-container m-3 flex flex-col gap-3 rounded border border-gray-600 bg-gray-700/[0.7] p-5"
 			>
-				<h1 class="eligibility-text-container font-LeagueSpartanFont text-xl font-semibold">
+				<h1 class="eligibility-text-container font-LeagueSpartanFont text-3xl font-bold">
 					Eligibility
 				</h1>
 				<div class="professions flex flex-row p-1 pl-0">
@@ -76,19 +76,19 @@
 						</div>
 					{/each}
 				</div>
-				<div class="elig-datas flex flex-wrap">
+				<div class="elig-datas flex flex-wrap pt-3">
 					{#each eligData2 as item}
-						<div class="team-regs mx-2 flex flex-row pb-2">
+						<div class="team-regs mr-5 flex flex-row pb-2">
 							<div
 								class="team-reg-icon g-1 flex h-[2.5rem] w-[2.5rem] justify-center rounded bg-gray-900/[0.5] p-2"
 							>
 								<i class="${item.icon} translate-y-[0.15rem]"></i>
 							</div>
 							<div class="team-reg-det flex flex-col">
-								<div class="team-reg-text px-1 font-LeagueSpartanFont text-[0.65rem]">
+								<div class="team-reg-text px-1 font-LeagueSpartanFont text-[0.8rem]">
 									{item.title}
 								</div>
-								<div class="team-reg-data mx-1 flex justify-start text-[0.85rem]">{item.des}</div>
+								<div class="team-reg-data mx-1 flex justify-start text-[0.95rem]">{item.des}</div>
 							</div>
 						</div>
 					{/each}
@@ -96,7 +96,7 @@
 			</div>
 		</div>
 
-		<div class="neurathon-info-container g-1 overflow-hidden md:w-[50%] lg:w-[50%] xl:w-[50%]">
+		<div class="neurathon-info-container g-1 overflow-hidden md:w-[60%] lg:w-[60%] xl:w-[60%]">
 			<div class="event-timeline m-3 rounded border border-gray-600 bg-gray-700/[0.5] pb-5">
 				<ParticipateTimeline />
 			</div>
--- src/lib/components/ui/ParticipateTimeline.svelte
+++ src/lib/components/ui/ParticipateTimeline.svelte
@@ -3,11 +3,10 @@
 </script>
 
 <div class="bg-transparent">
-	<div class="mx-auto ml-0 max-w-xl p-8 pl-7">
+	<div class="mx-auto ml-0 p-8 pl-7">
 		{#each data as item (item.version)}
 			<div class="relative pb-8">
-				<span class="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"
-				></span>
+				<span class="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true"></span>
 				<div class="relative flex items-start space-x-3">
 					<div>
 						<div class="relative px-1">
@@ -17,7 +16,7 @@
 					<div class="min-w-0 flex-1 py-0">
 						<div class="text-md text-gray-500">
 							<div>
-								<a href="/" class="mr-2 font-medium text-white">{item.version}</a>
+								<a href="/" class="mr-2 font-medium text-white text-2xl">{item.version}</a>
 								<a
 									href="/"
 									class="relative my-0.5 inline-flex items-center rounded-full border border-gray-300 bg-white px-3 py-0.5 text-sm"
@@ -36,9 +35,14 @@
 							<span class="whitespace-nowrap text-sm">{item.duration}</span>
 						</div>
 						<div class="mt-2 text-white">
-							{#each item.updates as update}
-								<p>{update}</p>
-							{/each}
+							<p>
+								{item.description}
+							</p>
+							<ul class="list-disc pl-5">
+								{#each item.updates as update}
+									<li>{update}</li>
+								{/each}
+							</ul>
 						</div>
 					</div>
 				</div>
