5bd289de5dc1d89dd969ebc5984443b4c0c29aff
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-01-25 09:39:36 +0530

    Profile Ui fixed

--- src/routes/dashboard/profile/+page.svelte
+++ src/routes/dashboard/profile/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section class="flex min-h-screen w-full flex-col items-center justify-center">
-	<nav class="flex w-full my-2" aria-label="Breadcrumb">
+	<nav class="my-2 flex w-full" aria-label="Breadcrumb">
 		<ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
 			<li class="inline-flex items-center">
 				<a
@@ -94,117 +94,149 @@
 			<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
 		</div>
 	</div>
-	<div
-		class="font-std mb-10 mt-3 w-full rounded-lg bg-[#ffac13] p-10 pt-5 font-normal leading-relaxed text-gray-900 shadow-xl"
-	>
-		<div class="flex flex-col">
-			<div class="mb-5 flex flex-col items-start justify-between md:flex-row">
-				<h2 class="text-3xl font-bold text-red-900">Update Profile</h2>
-			</div>
+	<div class="mx-auto w-full max-w-[800px]">
+		<div
+			class="group relative grid overflow-hidden rounded-xl px-8 py-6 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200"
+		>
+			<!-- Animated Gradient Background -->
+			<span>
+				<span
+					class="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-xl [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-kitrotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]"
+				></span>
+			</span>
 
-			<form class="space-y-4">
-				<div>
-					<label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-					<div
-						id="name"
-						class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-					>
-						{user?.user_metadata?.name}
-					</div>
-				</div>
-				<div>
-					<label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-					<div
-						id="email"
-						class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-					>
-						{user?.email}
-					</div>
-				</div>
-				<div>
-					<label for="phone" class="block text-sm font-medium text-gray-700">Phone</label>
-					<div
-						id="phone"
-						class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-					>
-						{user?.user_metadata?.phone}
-					</div>
-				</div>
-				{#if 0}
+			<!-- Backdrop for Content -->
+			<span
+				class="backdrop absolute inset-px rounded-[11px] bg-neutral-950 transition-colors duration-200"
+			></span>
+
+			<!-- Form Content -->
+			<div class="z-10 space-y-6">
+				<h2 class="text-3xl font-bold text-neutral-200">Update Profile</h2>
+
+				<form class="space-y-6">
+					<!-- Name Field -->
 					<div>
-						<label for="organization" class="block text-sm font-medium text-gray-700"
-							>Organization</label
+						<label for="name" class="mb-2 block text-sm font-medium text-neutral-400">Name</label>
+						<div
+							id="name"
+							class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
 						>
-						<input
-							type="text"
-							id="organization"
-							class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-							value="Estep Bilişim"
-						/>
+							{user?.user_metadata?.name}
+						</div>
 					</div>
+
+					<!-- Email Field -->
 					<div>
-						<label for="location" class="block text-sm font-medium text-gray-700">Location</label>
-						<input
-							type="text"
-							id="location"
-							class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-							value="San Francisco, CA"
-						/>
-					</div>
-					<div>
-						<label for="location" class="block text-sm font-medium text-gray-700">Location</label>
-						<input
-							type="text"
-							id="location"
-							class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-							value="San Francisco, CA"
-						/>
-					</div>
-				{:else}
-					<div>
-						<label for="location" class="block text-sm font-medium text-gray-700">Organization</label>
-						<input
-							type="text"
-							id="location"
-							class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-							value="San Francisco, CA"
-						/>
-					</div>
-					<div>
-						<label for="organization" class="block text-sm font-medium text-gray-700"
-							>Course</label
+						<label for="email" class="mb-2 block text-sm font-medium text-neutral-400">Email</label>
+						<div
+							id="email"
+							class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
 						>
-						<select
-							id="organization"
-							class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-						>
-						<option value="ug" selected>UG</option>
-						<option value="pg">PG</option>
-						</select>
+							{user?.email}
+						</div>
 					</div>
+
+					<!-- Phone Field -->
 					<div>
-						<label for="location" class="block text-sm font-medium text-gray-700">Location</label>
-						<input
-							type="text"
-							id="location"
-							class="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-indigo-500 focus:ring-indigo-500"
-							value="San Francisco, CA"
-						/>
+						<label for="phone" class="mb-2 block text-sm font-medium text-neutral-400">Phone</label>
+						<div
+							id="phone"
+							class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
+						>
+							{user?.user_metadata?.phone}
+						</div>
 					</div>
-				{/if}
-				<div class="flex justify-end space-x-4">
-					<button
-						onclick={() => goto('/dashboard/profile')}
-						type="button"
-						class="rounded-lg bg-gray-300 px-4 py-2 text-gray-700 hover:bg-gray-400">Cancel</button
-					>
-					<button
-						type="submit"
-						class="rounded-lg bg-indigo-800 px-4 py-2 text-white hover:bg-indigo-700"
-						>Save Changes</button
-					>
-				</div>
-			</form>
+
+					<!-- Conditional Fields -->
+					{#if 0}
+						<!-- Organization Field -->
+						<div>
+							<label for="organization" class="mb-2 block text-sm font-medium text-neutral-400"
+								>Organization</label
+							>
+							<input
+								type="text"
+								id="organization"
+								class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
+								value="Estep Bilişim"
+							/>
+						</div>
+
+						<!-- Location Field -->
+						<div>
+							<label for="location" class="mb-2 block text-sm font-medium text-neutral-400"
+								>Location</label
+							>
+							<input
+								type="text"
+								id="location"
+								class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
+								value="San Francisco, CA"
+							/>
+						</div>
+					{:else}
+						<!-- Organization Field -->
+						<div>
+							<label for="organization" class="mb-2 block text-sm font-medium text-neutral-400"
+								>Organization</label
+							>
+							<input
+								type="text"
+								id="organization"
+								class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
+								value=""
+								placeholder="Add your organisation (i.e., college, university, etc...)"
+							/>
+						</div>
+
+						<!-- Course Field -->
+						<div>
+							<label for="course" class="mb-2 block text-sm font-medium text-neutral-400"
+								>Course</label
+							>
+							<select
+								id="course"
+								class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
+							>
+								<option value="ug" selected>UG</option>
+								<option value="pg">PG</option>
+							</select>
+						</div>
+
+						<!-- College ID Submission Field -->
+						<div class="h-max flex flex-col">
+							<div class="mb-2 block text-sm font-medium text-neutral-400"
+								>Organization ID Submission</div
+							>
+							<label for="collegeId" class="w-full rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500">Choose file (pdf, jpg, jpeg, png supported)</label>
+							<input
+								type="file"
+								id="collegeId"
+								class="w-full hidden rounded-lg border border-neutral-700 bg-neutral-900 px-4 py-3 text-neutral-200 focus:border-indigo-500 focus:ring-indigo-500"
+								accept=".pdf,.jpeg,.jpg,.png"
+							/>
+						</div>
+					{/if}
+
+					<!-- Buttons -->
+					<div class="flex justify-end space-x-4">
+						<button
+							onclick={() => goto('/dashboard/profile')}
+							type="button"
+							class="rounded-lg bg-neutral-700 px-6 py-2 text-neutral-200 transition-colors duration-200 hover:bg-neutral-600"
+						>
+							Cancel
+						</button>
+						<button
+							type="submit"
+							class="rounded-lg bg-indigo-600 px-6 py-2 text-white transition-colors duration-200 hover:bg-indigo-700"
+						>
+							Save Changes
+						</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
 </section>
