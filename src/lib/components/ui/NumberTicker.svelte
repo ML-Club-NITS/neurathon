1610f049ecb2177fcba5ba401530e0befdf3aebc
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-01-27 20:07:45 +0530

    number ticker in prizes

---
+++ src/lib/components/ui/NumberTicker.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { cubicOut } from "svelte/easing";
+    import { onMount } from "svelte";
+    import { spring, tweened } from "svelte/motion";
+    import { cn } from "$lib/utils";
+    export let value = 100;
+    export let initial = 0;
+    export let duration = 3000;
+    let num = tweened(initial, {
+      duration: duration,
+      easing: cubicOut,
+    });
+    let className: any = "";
+    export { className as class };
+    onMount(() => {
+      num.set(value);
+    });
+  </script>
+  
+  <div
+    class={cn(
+      "inline-block  text-black dark:text-white tracking-normal font-LeagueSpartanFont font-bold text-3xl md:text-4xl",
+      className
+    )}
+    {...$$restProps}
+  >
+    {$num.toFixed(0)}+
+  </div>
+  
