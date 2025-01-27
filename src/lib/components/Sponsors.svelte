eae50739b4e10c0246fba375b9c394d3c0cea7fa
mlclubnits <mlclub.nits@gmail.com>
2025-01-27 16:39:15 +0530

    Add: gsap to Past Sponsers section

--- src/lib/components/Sponsors.svelte
+++ src/lib/components/Sponsors.svelte
@@ -8,48 +8,54 @@
 	let zeroRow = prevSponserData.sponsers.slice(0, sponserData.sponsers.length);
 	// let firstRow = sponserData.sponsers.slice(0, sponserData.sponsers.length / 2);
 	// let secondRow = sponserData.sponsers.slice(sponserData.sponsers.length / 2);
-
 	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 	gsap.registerPlugin(ScrollTrigger);
-
 	$effect(() => {
 		let tl = gsap.timeline();
-		tl.from('#sponsers, #inside_sponsers', {
+		tl.from('#sponsors, #inside_sponsers', {
 			opacity: 0,
-			scale: 0.6,
+			y: 60,
 			stagger: 1,
+			duration: 1.5,
 
 			scrollTrigger: {
-				trigger: '#sponsers',
-				scrub: 1.5,
-				start: 'top 40%',
-				end: 'bottom 90%',
-				once: true
-				// markers: true,
+				trigger: '#sponsors',
+				scrub: 1,
+				start: '-=400',
+				end: '+=10',
+				once: true,
+				snap: {
+					ease: 'power1.inOut'
+				}
+				// markers: true
 			}
 		}).from(
-			'#sponsers, #sponser_cards',
+			'#sponser_cards',
 			{
 				opacity: 0,
-				y: 50,
+				y: 60,
 				stagger: 1,
+				duration: 1,
 
 				scrollTrigger: {
-					trigger: '#sponsers',
-					scrub: 1.5,
-					start: 'top 40%',
-					end: 'bottom 90%',
-					once: true
-					// markers: true,
+					trigger: '#inside_sponsers',
+					scrub: 1,
+					start: '-=400',
+					end: '+=10',
+					once: true,
+					snap: {
+						ease: 'power1.inOut'
+					}
+					// markers: true
 				}
 			},
-			'-=0.4'
+			'-=0.1'
 		);
 	});
 </script>
 
-<div id="sponsors" class="sponser-container pt-28">
+<div id="sponsors" class="sponser-container pt-0 sm:pt-28">
 	<div class="sponser_intro" id="inside_sponsers">
 		<span
 			class="inline-flex w-fit animate-text-gradient text-wrap bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] bg-clip-text p-2 text-center text-2xl font-bold text-transparent md:text-3xl lg:text-5xl 2xl:text-8xl"
@@ -57,7 +63,7 @@
 			Past Sponsors
 		</span>
 	</div>
-	<div class="sponser_cards">
+	<div class="sponser_cards" id="sponser_cards">
 		<div
 			class="bg-background relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg py-1 md:shadow-xl"
 		>
