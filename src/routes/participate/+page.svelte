684bb78571585f754bc543772bec0b924f9e4d98
mlclubnits <mlclub.nits@gmail.com>
2025-01-29 11:31:17 +0530

    upd: number of registerations

--- src/routes/participate/+page.svelte
+++ src/routes/participate/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import Participate from '$lib/components/Participate.svelte';
+	import { onMount } from 'svelte';
 
 	let { data } = $props();
+
+	onMount(async () => {
+		try {
+			const response = await fetch('/participate');
+			if (response.ok) {
+				const result = await response.json();
+				data = result.data;
+			}
+		} catch (error) {
+			console.error('Error during fetch:', error);
+		}
+	});
 </script>
 
 <svelte:head>
